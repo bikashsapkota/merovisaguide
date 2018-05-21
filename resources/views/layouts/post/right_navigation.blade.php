117c5a52d48e6e1f869dfd71767a1a0750433dfd
UmeshDhungana <dhungana.umesh77@gmail.com>
2018-05-21 13:19:28 +0545

    Recent post Design Added, comments removed

--- resources/views/layouts/post/right_navigation.blade.php
+++ resources/views/layouts/post/right_navigation.blade.php
@@ -1,331 +1,69 @@
+<div style="padding-top: 100px">
+    <h3>Recent Posts</h3>
 
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
+    <ul class="list-group clear-list">
+        <li class="list-group-item fist-item">
+            <span class="pull-right"> 09:00 pm </span>
+            Please contact me
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 10:16 am </span>
+            Sign a contract
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 08:22 pm </span>
+            Open new shop
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 11:06 pm </span>
+            Call back to Sylvia
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 12:00 am </span>
+            Write a letter to Sandra
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 10:16 am </span>
+            Sign a contract
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 08:22 pm </span>
+            Open new shop
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 11:06 pm </span>
+            Call back to Sylvia
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 12:00 am </span>
+            Write a letter to Sandra
+        </li><li class="list-group-item">
+            <span class="pull-right"> 10:16 am </span>
+            Sign a contract
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 08:22 pm </span>
+            Open new shop
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 11:06 pm </span>
+            Call back to Sylvia
+        </li>
+        <li class="list-group-item">
+            <span class="pull-right"> 12:00 am </span>
+            Write a letter to Sandra
+        </li>
+    </ul>
 
 
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
-
-    A
-    B
-    C
-    asdads
-    A
-    B
-    C
-    asdadsA
-    B
-    C
-    asdadsA
-    B
-    C
-    asdads
+<!--    <h5>Project tag</h5>-->
+<!--    <ul class="tag-list" style="padding: 0">-->
+<!--        <li><a href=""><i class="fa fa-tag"></i> Zender</a></li>-->
+<!--        <li><a href=""><i class="fa fa-tag"></i> Lorem ipsum</a></li>-->
+<!--        <li><a href=""><i class="fa fa-tag"></i> Passages</a></li>-->
+<!--        <li><a href=""><i class="fa fa-tag"></i> Variations</a></li>-->
+<!--    </ul>-->
+    <div class="text-center m-t-md">
+        <a href="#" class="btn btn-xs btn-primary">More</a>
+    </div>
+</div>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -14,7 +14,7 @@
                             <div class="text-center article-title">
                             <span class="text-muted"><i class="fa fa-clock-o"></i> 28th Oct 2015</span>
                                 <h1>
-                                    How to apply student visa in Austrellia
+                                    How to apply student visa in Australia
                                 </h1>
                             </div>
                             <p>
@@ -37,113 +37,7 @@
                             <p>
                                 Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex.
                             </p>
-                            <hr>
-                            <div class="row">
-                                <div class="col-md-6">
-                                        <h5>Tags:</h5>
-                                        <button class="btn btn-primary btn-xs" type="button">Model</button>
-                                        <button class="btn btn-white btn-xs" type="button">Publishing</button>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="small text-right">
-                                        <h5>Stats:</h5>
-                                        <div> <i class="fa fa-comments-o"> </i> 56 comments </div>
-                                        <i class="fa fa-eye"> </i> 144 views
-                                    </div>
-                                </div>
-                            </div>
 
-                            <div class="row">
-                                <div class="col-lg-12">
-
-                                    <h2>Comments:</h2>
-                                    <div class="social-feed-box">
-                                        <div class="social-avatar">
-                                            <a href="" class="pull-left">
-                                                <img alt="image" src="img/a1.jpg">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    Andrew Williams
-                                                </a>
-                                                <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
-                                            </div>
-                                        </div>
-                                        <div class="social-body">
-                                            <p>
-                                                Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-                                                default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                                default model text.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="social-feed-box">
-                                        <div class="social-avatar">
-                                            <a href="" class="pull-left">
-                                                <img alt="image" src="img/a2.jpg">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    Michael Novek
-                                                </a>
-                                                <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
-                                            </div>
-                                        </div>
-                                        <div class="social-body">
-                                            <p>
-                                                Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-                                                default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                                default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                                in their infancy. Packages and web page editors now use Lorem Ipsum as their
-                                                default model text.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="social-feed-box">
-                                        <div class="social-avatar">
-                                            <a href="" class="pull-left">
-                                                <img alt="image" src="img/a3.jpg">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    Alice Mediater
-                                                </a>
-                                                <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
-                                            </div>
-                                        </div>
-                                        <div class="social-body">
-                                            <p>
-                                                Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-                                                default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                                in their infancy. Packages and web page editors now use Lorem Ipsum as their
-                                                default model text.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="social-feed-box">
-                                        <div class="social-avatar">
-                                            <a href="" class="pull-left">
-                                                <img alt="image" src="img/a5.jpg">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    Monica Flex
-                                                </a>
-                                                <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
-                                            </div>
-                                        </div>
-                                        <div class="social-body">
-                                            <p>
-                                                Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-                                                default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                                in their infancy. Packages and web page editors now use Lorem Ipsum as their
-                                                default model text.
-                                            </p>
-                                        </div>
-                                    </div>
-
-
-                                </div>
                             </div>
 
 
@@ -151,8 +45,6 @@
                     </div>
                 </div>
             </div>
-
-
-        </div>
+    
 </section>
 @endsection
