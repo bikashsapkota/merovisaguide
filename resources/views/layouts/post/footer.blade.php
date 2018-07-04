db48103d39ab1ff99b9fa8f1c832b507727524dc
UmeshDhungana <dhungana.umesh77@gmail.com>
2018-07-04 22:24:32 +0545

    Footer Completed

--- resources/views/layouts/post/footer.blade.php
+++ resources/views/layouts/post/footer.blade.php
@@ -1,47 +1,74 @@
-<section id="contact" class="gray-section contact">
+<section id="contact" class="gray-section contact" xmlns="http://www.w3.org/1999/html"
+         xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
     <div class="container">
         <div class="row m-b-lg">
             <div class="col-lg-12 text-center">
                 <div class="navy-line"></div>
                 <h1>Contact Us</h1>
-                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
             </div>
         </div>
-        <div class="row m-b-lg">
-            <div class="col-lg-3 col-lg-offset-3">
-                <address>
-                    <strong><span class="navy">Company name, Inc.</span></strong><br/>
-                    795 Folsom Ave, Suite 600<br/>
-                    San Francisco, CA 94107<br/>
-                    <abbr title="Phone">P:</abbr> (123) 456-7890
-                </address>
-            </div>
-            <div class="col-lg-4">
-                <p class="text-color">
-                    Consectetur adipisicing elit. Aut eaque, totam corporis laboriosam veritatis quis ad perspiciatis, totam corporis laboriosam veritatis, consectetur adipisicing elit quos non quis ad perspiciatis, totam corporis ea,
-                </p>
-            </div>
-        </div>
+
         <div class="row">
-            <div class="col-lg-12 text-center">
-                <a href="mailto:test@email.com" class="btn btn-primary">Send us mail</a>
-                <p class="m-t-sm">
-                    Or follow us on social platform
-                </p>
+            <div class="col-lg-3 text-center">
+<!--                <a href="mailto:test@email.com" class="btn btn-primary">Send us mail</a>-->
+                    <strong><span class="navy">Follow Us</span></strong>
                 <ul class="list-inline social-icon">
                     <li><a href="#"><i class="fa fa-twitter"></i></a>
                     </li>
                     <li><a href="#"><i class="fa fa-facebook"></i></a>
                     </li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a>
                     </li>
                 </ul>
             </div>
+            <div class="col-lg-3 text-center">
+                <address>
+                    <strong><span class="navy text-lg-center">Mero Visa Guide</span></strong><br/>
+                    Kathmandu, Nepal<br/>
+                </address>
+            </div>
+            <div class="col-lg-3 text-center">
+                <strong><span class="navy text-lg-center">Phone</span></strong><br/>
+                +977-9843922847
+            </div>
+            <div class="col-lg-3 text-center">
+                <strong><span class="navy text-lg-center">Email</span></strong><br/>
+                hello@mvg.com
+            </div>
+
         </div>
+
+        <hr>
+
+        <div class="row m-b-lg">
+            <div class="col-lg-3">
+                <ul class="product-desc list-unstyled">
+                    <u style="font-size: 18px">ABOUT MVG</u></br></br>
+                    <li><a class="footer-link" href=""> FAQs</a></li>
+                    <li><a class="footer-link" href=""> About Us</a></li>
+                    <li><a class="footer-link" href=""> Terms and Conditions</a></li>
+                    <li><a class="footer-link" href=""> Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <ul class="product-desc list-unstyled">
+                    <u style="font-size: 18px">OUR SERVICES</u></br></br>
+                    <li><a class="footer-link" href=""> Visa Guidance</a></li>
+                    <li><a class="footer-link" href=""> Visa Processing</a></li>
+                    <li><a class="footer-link" href=""> Free Consultation</a></li>
+                    <li><a class="footer-link" href=""> Post Visa Counselling</a></li>
+                    <li><a class="footer-link" href=""> Insurance Guidelines</a></li>
+                    <li><a class="footer-link" href=""> Documentation</a></li>
+                </ul>
+            </div>
+        </div>
+
         <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 text-center m-t-lg m-b-lg">
-                <p><strong>&copy; 2015 Company Name</strong><br/> consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+            <div class="col-lg-8 col-lg-offset-2 text-center m-b-lg">
+                <p><strong>&copy; 2018 Mero Visa Guide</strong><br/> All information and contents of this site and pages included within it are Copyrighted ©2018 by Mero Visa Guide Inc. Any use of material from this site without expressed consent from Mero Visa Guide is illegal and punishable by law. </p>
             </div>
         </div>
     </div>
 </section>
+
+<script src="js/plugins/i18next/i18next.min.js"></script>
